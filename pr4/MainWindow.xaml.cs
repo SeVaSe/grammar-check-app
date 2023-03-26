7d06e685f640543fd2c3639cc58e09385f41b48f
sevase <108822198+SeVaSe@users.noreply.github.com>
2023-03-26 21:07:55 +0300

    grammar

---
+++ pr4/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Office.Interop.Word;
+
+
+namespace pr4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : System.Windows.Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Office.Interop.Word.Application wordApp = new Microsoft.Office.Interop.Word.Application();
+            Document doc = wordApp.Documents.Add();
+            Range range = doc.Range();
+
+            range.Text = Txt_box.Text;
+            int err = doc.SpellingErrors.Count;
+
+            if (range.SpellingErrors.Count > 0)
+            {
+                string msg = "Орфографические ошибки: \n";
+                foreach (Range error in range.SpellingErrors)
+                {
+                    msg += error.Text + "\n";
+                }
+                MessageBox.Show(msg, $"Найдены ошибки в орфографии, количество: {err}");
+            }
+            else
+            {
+                MessageBox.Show("Ошибок в тексте нет", "Корректный синтаксис");
+            }
+
+        }
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*Microsoft.Office.Interop.Word.Application wordApp = new Microsoft.Office.Interop.Word.Application();
+
+        Document doc = wordApp.Documents.Add();
+        string text = Txt_box.Text;
+
+        doc.Content.Text = text;
+            doc.CheckSpelling();
+            int errorDoc = doc.SpellingErrors.Count;
+
+            if (errorDoc == 0)
+            {
+                MessageBox.Show("Ошибок в тексте нет", "Корректный синтаксис");
+            }
+            else
+            {
+                MessageBox.Show($"Обнаружены орфографические ошибки в тексте, количество: {errorDoc}", "Не корректный синтаксис");
+            }*/
+
+
+
+    }
+}
